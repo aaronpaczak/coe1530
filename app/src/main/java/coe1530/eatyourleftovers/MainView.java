5756a7036e47c5d0b198d2481fdf8514e006d51b
Rachel Misbin <rachelmisbin@pitt.edu>
2018-10-26 12:38:52 -0400

    Renamed xml variables from old template convention to reflect the application (calendar, todo, and map respectively)

--- app/src/main/java/coe1530/eatyourleftovers/MainView.java
+++ app/src/main/java/coe1530/eatyourleftovers/MainView.java
@@ -17,14 +17,14 @@ public class MainView extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_calendar:
+                    mTextMessage.setText(R.string.title_calendar);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_todo:
+                    mTextMessage.setText(R.string.title_todo);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_map:
+                    mTextMessage.setText(R.string.title_map);
                     return true;
             }
             return false;
