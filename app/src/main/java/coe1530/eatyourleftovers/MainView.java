bbcaf30b0291c7c4a7e1c87f2d8e6d249180581b
Aaron Paczak <aaronpaczak@Aarons-MacBook-Pro.local>
2018-11-02 11:53:50 -0400

    minimal fragment ex

--- app/src/main/java/coe1530/eatyourleftovers/MainView.java
+++ app/src/main/java/coe1530/eatyourleftovers/MainView.java
@@ -3,13 +3,19 @@ package coe1530.eatyourleftovers;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainView extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private BottomNavigationView navigation;
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,26 +24,33 @@ public class MainView extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_calendar:
-                    mTextMessage.setText(R.string.title_calendar);
-                    return true;
+                    fragment = new CalendarFragment();
+                    break;
                 case R.id.navigation_todo:
-                    mTextMessage.setText(R.string.title_todo);
-                    return true;
+                    fragment = new TodoFragment();
+                    break;
                 case R.id.navigation_map:
-                    mTextMessage.setText(R.string.title_map);
-                    return true;
+                    fragment = new MapFragment();
+                    break;
             }
-            return false;
+            final FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.main_container, fragment).commit();
+            return true;
         }
     };
 
     @Override
+    // Code that is run when this activity has begun
+    // for this one, since it is the main activity, the is called when the launcher launches this app
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Blows up what belongs in the xml file whose xml file is "layout/activity_main_view"
         setContentView(R.layout.activity_main_view);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.inflateMenu(R.menu.navigation);
+        fragmentManager = getSupportFragmentManager();
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
