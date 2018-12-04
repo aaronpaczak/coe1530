c027c4ca65abefda5fe2e3b0cf489e70f3d4e864
colihanvm <vanessa.colihan@gmail.com>
2018-12-04 18:52:10 -0500

    yippee

--- app/src/main/java/coe1530/eatyourleftovers/HelpFragment.java
+++ app/src/main/java/coe1530/eatyourleftovers/HelpFragment.java
@@ -1,5 +1,13 @@
 package coe1530.eatyourleftovers;
 
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.view.WindowManager;
+import android.view.KeyEvent;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,99 +16,58 @@ import android.view.ViewGroup;
 
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link HelpFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link HelpFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * A simple help page fragment subclass
  */
-public class HelpFragment extends Fragment {
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-//
-//    private OnFragmentInteractionListener mListener;
-//
-//    public HelpFragment() {
-//        // Required empty public constructor
-//    }
-//
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment HelpFragment.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static HelpFragment newInstance(String param1, String param2) {
-//        HelpFragment fragment = new HelpFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
+public class HelpFragment extends Fragment implements View.OnClickListener {
+
+    private TextInputEditText sendTxt;
+    private String sendString;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+
+        super.onCreate(savedInstanceState);
+
+        //getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+
+        View view = inflater.inflate(R.layout.fragment_help, container, false);
+        sendTxt = (TextInputEditText) view.findViewById(R.id.sendEmailTxt);
+        //sendString = sendTxt.getText().toString();
+        Button sendBtn = (Button) view.findViewById(R.id.sendEmailBtn);
+        sendBtn.setOnClickListener(this);
+
+        return view;
     }
-//
-//    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
-//
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
-//
-//    /**
-//     * This interface must be implemented by activities that contain this
-//     * fragment to allow an interaction in this fragment to be communicated
-//     * to the activity and potentially other fragments contained in that
-//     * activity.
-//     * <p>
-//     * See the Android Training lesson <a href=
-//     * "http://developer.android.com/training/basics/fragments/communicating.html"
-//     * >Communicating with Other Fragments</a> for more information.
-//     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            // If you click the submit button
+            case R.id.sendEmailBtn:
+                sendString = sendTxt.getText().toString();
+                sendEmail();
+                sendTxt.setText("");
+        }
+    }
+
+    protected void sendEmail() {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"Eat@Your.Leftovers"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "Hi!");
+        i.putExtra(Intent.EXTRA_TEXT, sendString);
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this.getActivity(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
