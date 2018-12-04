245a8b318ef3660936b16fccb0fa5aa5259b4e52
Rachel Misbin <rachelmisbin@pitt.edu>
2018-12-04 17:49:29 -0500

    Added simple notification

--- app/src/main/java/coe1530/eatyourleftovers/MainView.java
+++ app/src/main/java/coe1530/eatyourleftovers/MainView.java
@@ -1,13 +1,17 @@
 package coe1530.eatyourleftovers;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.roomorama.caldroid.CaldroidFragment;
@@ -62,5 +66,22 @@ public class MainView extends AppCompatActivity implements TodoItemFragment.OnLi
     @Override
     public void onListFragmentInteraction(ToDoList.ToDoItem item) {
 
+    }
+
+    public void sendNotification(View view)
+    {
+
+        //Get an instance of NotificationManager
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.notification)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+
+
+        // Gets an instance of the NotificationManager service
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(001, mBuilder.build());
     }
 }
